a2234b270f414fdcd44bae3d0709705fd0ea525a
Teia Leusten <teia.leusten@proton.me>
2024-04-15 18:45:43 +0300

    Implement modular equality comparisons

--- src/lib.rs
+++ src/lib.rs
@@ -113,6 +113,29 @@ where
     }
 }
 
+pub trait EqMod<M: Modulus<Self>, Rhs = Self>
+where
+    Self: TryFrom<M>,
+    <Self as TryFrom<M>>::Error: std::fmt::Debug,
+{
+    fn eq_mod(self, rhs: Rhs, modulus: M) -> bool;
+    fn ne_mod(self, rhs: Rhs, modulus: M) -> bool;
+}
+
+impl<T, M> EqMod<M> for T
+where
+    T: Integer + TryFrom<M> + Constrain<M>,
+    <T as TryFrom<M>>::Error: std::fmt::Debug,
+    M: Modulus<T>,
+{
+    fn eq_mod(self, rhs: T, modulus: M) -> bool {
+        self.constrain(modulus) == rhs.constrain(modulus)
+    }
+    fn ne_mod(self, rhs: T, modulus: M) -> bool {
+        self.constrain(modulus) != rhs.constrain(modulus)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -188,5 +211,33 @@ mod tests {
         assert_eq!((-10).div_mod(5, 11u8), Some(9));
         assert_eq!((-10).div_mod(5, 13u8), Some(11));
         assert_eq!((-10).div_mod(5, 10u8), None);
+    }
+
+    #[test]
+    fn test_eq_mod() {
+        assert!(10.eq_mod(3, 7u8));
+        assert!(10.eq_mod(10, 11u8));
+        assert!(10.eq_mod(10, 13u8));
+    }
+
+    #[test]
+    fn test_eq_negative_mod() {
+        assert!((-10).eq_mod(4, 7u8));
+        assert!((-10).eq_mod(0, 10u8));
+        assert!((-10).eq_mod(3, 13u8));
+    }
+
+    #[test]
+    fn test_ne_mod() {
+        assert!(!10.ne_mod(3, 7u8));
+        assert!(!10.ne_mod(0, 10u8));
+        assert!(!10.ne_mod(10, 13u8));
+    }
+
+    #[test]
+    fn test_ne_negative_mod() {
+        assert!(!(-10).ne_mod(4, 7u8));
+        assert!(!(-10).ne_mod(0, 10u8));
+        assert!(!(-10).ne_mod(3, 13u8));
     }
 }
