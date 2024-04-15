45a2a4484961c38c06154ec74cadbc0a9febe0d4
Teia Leusten <teia.leusten@proton.me>
2024-04-15 18:39:43 +0300

    Implement modular inverse and division

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 mod egcd;
 mod integer;
+mod invert;
 mod modulus;
 
+pub use egcd::Egcd;
 pub use integer::Integer;
+pub use invert::Invert;
 pub use modulus::Modulus;
 
 pub trait Constrain<M: Modulus<Self>>
@@ -88,6 +91,28 @@ where
     }
 }
 
+pub trait DivMod<M: Modulus<Self>, Rhs = Self>
+where
+    Self: TryFrom<M>,
+    <Self as TryFrom<M>>::Error: std::fmt::Debug,
+{
+    type Output;
+    fn div_mod(self, rhs: Rhs, modulus: M) -> Option<Self::Output>;
+}
+
+impl<T, M> DivMod<M> for T
+where
+    T: Invert + TryFrom<M>,
+    <T as TryFrom<M>>::Error: std::fmt::Debug,
+    M: Modulus<T>,
+{
+    type Output = T;
+    fn div_mod(self, rhs: T, modulus: M) -> Option<T> {
+        let inverse = rhs.invert(modulus)?;
+        Some((inverse * self).constrain(modulus))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -129,9 +154,39 @@ mod tests {
     }
 
     #[test]
+    fn test_sub_negative_mod() {
+        assert_eq!((-10).sub_mod(5, 7u8), 6);
+        assert_eq!((-10).sub_mod(5, 11u8), 7);
+        assert_eq!((-10).sub_mod(5, 13u8), 11);
+    }
+
+    #[test]
     fn test_mul_mod() {
         assert_eq!(10.mul_mod(5, 7u8), 1);
         assert_eq!(10.mul_mod(5, 11u8), 6);
         assert_eq!(10.mul_mod(5, 13u8), 11);
+    }
+
+    #[test]
+    fn test_mul_negative_mod() {
+        assert_eq!((-10).mul_mod(5, 7u8), 6);
+        assert_eq!((-10).mul_mod(5, 11u8), 5);
+        assert_eq!((-10).mul_mod(5, 13u8), 2);
+    }
+
+    #[test]
+    fn test_div_mod() {
+        assert_eq!(10.div_mod(5, 7u8), Some(2));
+        assert_eq!(10.div_mod(5, 11u8), Some(2));
+        assert_eq!(10.div_mod(5, 13u8), Some(2));
+        assert_eq!(10.div_mod(5, 10u8), None);
+    }
+
+    #[test]
+    fn test_div_negative_mod() {
+        assert_eq!((-10).div_mod(5, 7u8), Some(5));
+        assert_eq!((-10).div_mod(5, 11u8), Some(9));
+        assert_eq!((-10).div_mod(5, 13u8), Some(11));
+        assert_eq!((-10).div_mod(5, 10u8), None);
     }
 }
