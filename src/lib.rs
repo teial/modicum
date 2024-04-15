c4cc617c77212ce8ffbad4d0fd8819852867733e
Teia Leusten <teia.leusten@proton.me>
2024-04-15 19:36:01 +0300

    Implement exponentiation

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub use egcd::Egcd;
 pub use integer::Integer;
 pub use invert::Invert;
 pub use modulus::Modulus;
+use num_traits::FromPrimitive;
 
 pub trait Constrain<M: Modulus<Self>>
 where
@@ -110,6 +111,37 @@ where
     fn div_mod(self, rhs: T, modulus: M) -> Option<T> {
         let inverse = rhs.invert(modulus)?;
         Some((inverse * self).constrain(modulus))
+    }
+}
+
+pub trait PowMod<M: Modulus<Self>, Rhs = Self>
+where
+    Self: TryFrom<M>,
+    <Self as TryFrom<M>>::Error: std::fmt::Debug,
+{
+    type Output;
+    fn pow_mod(self, rhs: Rhs, modulus: M) -> Self::Output;
+}
+
+impl<T, M> PowMod<M> for T
+where
+    T: Integer + TryFrom<M> + Constrain<M> + FromPrimitive + MulMod<M, Output = T> + DivMod<M>,
+    <T as TryFrom<M>>::Error: std::fmt::Debug,
+    M: Modulus<T>,
+{
+    type Output = T;
+    fn pow_mod(self, mut rhs: T, modulus: M) -> T {
+        let two = T::from_i8(2).expect("two");
+        let mut result = T::one();
+        let mut base = self;
+        while rhs != T::zero() {
+            if rhs % two == T::one() {
+                result = result.mul_mod(base, modulus);
+            }
+            base = base.mul_mod(base, modulus);
+            rhs = rhs / two;
+        }
+        result
     }
 }
 
@@ -239,5 +271,23 @@ mod tests {
         assert!(!(-10).ne_mod(4, 7u8));
         assert!(!(-10).ne_mod(0, 10u8));
         assert!(!(-10).ne_mod(3, 13u8));
+    }
+
+    #[test]
+    fn test_pow_mod() {
+        assert_eq!(10.pow_mod(3, 3u8), 1);
+        assert_eq!(10.pow_mod(3, 7u8), 6);
+        assert_eq!(10.pow_mod(3, 10u8), 0);
+        assert_eq!(10.pow_mod(3, 11u8), 10);
+        assert_eq!(10.pow_mod(3, 13u8), 12);
+    }
+
+    #[test]
+    fn test_pow_negative_mod() {
+        assert_eq!((-10).pow_mod(3, 3u8), 2);
+        assert_eq!((-10).pow_mod(3, 7u8), 1);
+        assert_eq!((-10).pow_mod(3, 10u8), 0);
+        assert_eq!((-10).pow_mod(3, 11u8), 1);
+        assert_eq!((-10).pow_mod(3, 13u8), 1);
     }
 }
