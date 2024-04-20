12020a9a81a6248032b176073b6c1b23bbde424a
Teia Leusten <teia.leusten@proton.me>
2024-04-20 13:08:13 +0300

    Add documentation

--- src/modulus.rs
+++ src/modulus.rs
@@ -2,11 +2,16 @@ use num_traits::Unsigned;
 
 use crate::Integer;
 
+/// Modulus is an unsigned integer that can be cast to some other type `T`.
+/// The purpose is to restrict the modulus to unsigned integers yet allow calculations with signed integers
+/// when necessary.
 pub trait Modulus<T>: Integer + Unsigned
 where
     T: TryFrom<Self>,
     <T as TryFrom<Self>>::Error: std::fmt::Debug,
 {
+    /// Cast the modulus to some other type `T`.
+    /// Panics if the modulus cannot be converted to `T`.
     fn cast(self) -> T {
         self.try_into().expect("cannot convert modulus")
     }
